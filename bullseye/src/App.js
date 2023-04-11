01a96bdda2058a68fd2b28d27e16187e4ebbc8de
Abellarr <da.ortiz83@gmail.com>
2023-04-11 10:31:46 -0700

    Updated Carousel components to house three carousels with products. Moved the state and api call to the parent CarouselContainer component. Tested and displaying properly.

--- bullseye/src/App.js
+++ bullseye/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from "./components/header.jsx"
+import Header from "./components/header.jsx";
+import CarouselContainer from './components/carousel/CarouselContainer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CarouselContainer />
     </div>
   );
 }
---
+++ bullseye/src/components/carousel/Carousel1.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card.jsx";
+import Loading from "./Loading.jsx";
+import Slider from 'react-slick';
+import imageUrls from "./imgUrls.js";
+import './Carousel.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Carousel1 = (props) => {
+    const products = props.prod
+
+    const settings = {
+        accessibility: true,
+        dots: true,
+        arrows: true,
+        dotsClass: 'slick-dots',
+        adaptiveHeight: true,
+        slidesToShow: 7,
+        slidesToScroll: 5,
+        slide: 'div',
+        draggable: false
+    }
+    
+    return (
+        <div className="carousel">
+            <h2 className="main-carousel-header">Similar items</h2>
+            {(products.length <= 0) ? <div><Loading /></div> :
+            <div className="carousel-main">
+                <Slider {...settings}>
+                    <div>
+                        <Card image={imageUrls[0]} prod={products[1]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[1]} prod={products[2]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[2]} prod={products[3]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[3]} prod={products[4]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[4]} prod={products[5]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[5]} prod={products[6]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[6]} prod={products[7]} />
+                    </div>
+                    <div>
+                    <Card image={imageUrls[7]} prod={products[8]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[8]} prod={products[9]} />
+                    </div>
+                    <div>
+                        <Card image={imageUrls[9]} prod={products[10]} />
+                    </div>
+                </Slider>
+            </div>}
+        </div>
+    )
+}
+
+export default Carousel1;
