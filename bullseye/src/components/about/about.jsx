b172184a00b514e235dd38373ce874dbeba8122f
Never404s <kdsossa@gmail.com>
2023-04-13 02:22:38 -0500

    saving

---
+++ bullseye/src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Photo = () => {
+
+    return (
+        <div>
+            <div className="main-container">
+            
+            </div>
+        </div>
+    )
+}
