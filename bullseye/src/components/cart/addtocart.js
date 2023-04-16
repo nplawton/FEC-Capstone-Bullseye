3a6bf1be957cdec0a4d74aff8b9455cd9840043e
Shuyi Hoo <anloveshu@Shuyis-MacBook-Air.local>
2023-04-16 11:22:14 -0700

    changed cart and have items inside the button

--- bullseye/src/components/cart/addtocart.js
+++ bullseye/src/components/cart/addtocart.js
@@ -1,61 +1,111 @@
 import React, { useContext, useState } from "react";
 import { CartContext } from "./cartContext";
-import './Cart.css';
+import "./Cart.css";
 
 export const AddToCartButton = ({ item, quantity, toggleSidebar }) => {
-  const { addToCart } = useContext(CartContext);
-  const { cartItems, removeFromCart } = useContext(CartContext);
-  const [sidebarVisible, setSidebarVisible] = useState(false); // State to manage sidebar visibility
+  const { addToCart, cartItems } = useContext(CartContext);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [itemAdded, setItemAdded] = useState(false);
+  const [itemCount, setItemCount] = useState(0);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
-  const handleClick = () => {
-    addToCart(item, parseInt(quantity)); // Update the addToCart function call with quantity
-    setSidebarVisible(true); // Show the sidebar when an item is added to the cart
-    //toggleSidebar(); 
+  const handleClick = (newQuantity) => {
+    addToCart(item, parseInt(newQuantity));
+    setSidebarVisible(true);
+    setItemAdded(true);
+    setItemCount((prevCount) => prevCount + parseInt(newQuantity));
+    setDropdownVisible(false);
   };
+
+  const handleQuantityChange = (newQuantity) => {
+    if (newQuantity === 0) {
+      setItemAdded(false);
+      setItemCount(0);
+    } else {
+      handleClick(newQuantity);
+    }
+    setDropdownVisible(false);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   const handleCheckout = () => {
-    // Add your checkout logic here
-    console.log('Proceed to checkout');
+    console.log("Proceed to checkout");
   };
+  
   const toggleSidebar2 = () => {
     setSidebarVisible(!sidebarVisible); // Toggle the sidebar visibility
   };
 
+  const handleContinueShopping = () => {
+    setSidebarVisible(false);
+  };
+
   return (
     <>
-      <button className="cart2-btn" onClick={handleClick}>
-        Add to cart
-      </button>
+      {itemAdded ? (
+        <button className="cart2-btn green-outlined" onClick={toggleDropdown}>
+          {itemCount} in cart 
+        </button>
+      ) : (
+        <button className="cart2-btn" onClick={() => handleClick(quantity)}>
+          Add to cart
+        </button>
+      )}
+
+      {dropdownVisible && (
+        <div className="quantity-dropdown">
+          {[...Array(3).keys()].map((q) => (
+            <div
+              key={q}
+              onClick={() => handleQuantityChange(q)}
+              className="quantity-option">
+              {q}
+              </div>
+              ))}
+        </div>
+      )}
+
+      {/* ... the rest of your code ... */}
       <div className={`overlay ${sidebarVisible ? "visible" : ""}`} onClick={toggleSidebar}></div>
-      <div className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
+       <div className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
         <button className="close-btn" onClick={toggleSidebar2}>×</button> {/* Add the close button */}
-        <ul>
+         <ul>
           {cartItems.map((cartItem) => {
             // const totalPrice = (cartItem.price * cartItem.quantity).toFixed(2);
 
             return (
               <>
-              <div className='cart-text'>Added to cart</div>
+              <img alt="D7500" src="store1.png" width="100px"/>
+             
+              <div className='cart4-text'>Added to cart</div>
               <div key={cartItem.id}>
-              <div className='sidebarText'>Standard Shipping</div>
+              <div className='cart4-text'>Standard Shipping</div>
               <div className='cart3-text'>Get it by Monday Apr 24</div>
-              
-              
-              <div className="cartcontainer"><img alt="Nikon D7500 DX-Format DSLR Camera (Body Only, Black)" src="https://target.scene7.com/is/image/Target//GUEST_6e39668a-bb54-4e62-ad6b-0e065b1ed33c" width="100px"/>
+              <div className="cartcontainer">
+                <img alt="Nikon D7500" src="https://target.scene7.com/is/image/Target//GUEST_6e39668a-bb54-4e62-ad6b-0e065b1ed33c" width="100px"/>
               <div>
-                <p><span style={{ fontWeight: 'bold', color: "#000000" }}>   {cartItem.name}</span></p>
-                <span style={{ fontWeight: 'bold', color: "#B85300" }}> WARNING: </span>
-                <span style={{ fontSize: '9',color: '#B85300' }}> choking hazard - small parts. Not for children under 3 yrs. </span>
-                
+                <p>
+                  <span style={{fontWeight: 'bold', color: "#000000"}}>   {cartItem.name}</span></p>
+                  <span style={{fontWeight: 'bold', color: "#B85300"}}> WARNING: </span>
+                  <span style={{fontSize: '9',color: '#B85300'}}> choking hazard - small parts. Not for children under 3 yrs. </span>
                 </div>
                 </div>
                  {/* Quantity: {cartItem.quantity} - Total: ${totalPrice} */}
-                <button onClick={() => removeFromCart(cartItem)}>Remove</button>
+                {/* <button onClick={() => removeFromCart(cartItem)}>Remove</button> */}
               </div>
               </>
             );
           })}
         </ul>
-        <button className="cart2-btn" onClick={handleCheckout}>Checkout</button> {/* Add the checkout button */}
+      <button className="cart2-btn" onClick={handleContinueShopping}>
+        Continue shopping
+      </button>
+      <button className="cart2-btn" onClick={handleCheckout}>
+        Checkout
+      </button>
       </div>
     </>
   );
--- bullseye/src/components/cart/cart.js
+++ bullseye/src/components/cart/cart.js
@@ -4,19 +4,17 @@ import { AddToCartButton } from "./addtocart";
 import ProductContext from "../context/ProductContext.js";
 import { useContext, useState } from "react";
 import Loading from "../carousel/Loading.jsx";
-import { RoundedButton } from "./roundedButton";
+
 
 function Addcart() {
   const { products } = useContext(ProductContext);
   const items = [products[0]];
   const [quantity, setQuantity] = useState(1); // State for quantity
   const [sidebarVisible, setSidebarVisible] = useState(false); // State for sidebar visibility
+  const [itemAdded, setItemAdded] = useState(false); 
 
   const toggleSidebar = () => {
     setSidebarVisible(!sidebarVisible);
-  };
-  const handleButtonClick = (buttonLabel) => {
-    console.log("Button clicked:", buttonLabel);
   };
 
   return (
@@ -31,32 +29,34 @@ function Addcart() {
                 <h3>{item.name}</h3>
                 <p className="cart2-text">${item.price}</p>
                 <p>When purchased online</p>
-                <p>
-                  <RoundedButton label="Pick up" onClick={() => handleButtonClick("Button 1")} />
-                  <RoundedButton label="Delivery" onClick={() => handleButtonClick("Button 2")} />
-                  <RoundedButton label="Shipping" onClick={() => handleButtonClick("Button 3")} />
-                </p>
+               
                 <p>Ship to 98004</p>
                 <p className="cart3-text">Get it by Tue</p>
                 {/* Dropdown menu to update quantity */}
+                {!itemAdded && (
+                <>
                 <label htmlFor="quantity">Qty: </label>
                 <select
-                  value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
                 >
                   {/* Drop Down menu of quantity to be added to cart */}
-                  {[...Array(5)].map((_, index) => (
+                  {[...Array(2)].map((_, index) => (
                     <option key={index + 1} value={index + 1}>
                       {index + 1}
                     </option>
                   ))}
                 </select>
+                </>
+                  )}
                 
                 {/* Pass quantity and toggleSidebar to AddToCartButton */}
                 <AddToCartButton
                   item={item}
                   quantity={quantity}
                   toggleSidebar={toggleSidebar}
+                  setItemAdded={setItemAdded} // <-- Pass setItemAdded as a prop
+                
                 />
               </div>
             ))
